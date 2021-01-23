c3eea48f3bfb8bd44adf1420ed11db73002049e2
calt08 <cesarlopez2808@gmail.com>
2021-01-23 17:36:41 -0400

    Azure integration

--- StoreSystem/Startup.cs
+++ StoreSystem/Startup.cs
@@ -1,55 +1,44 @@
+using System.Text;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 using StoreSystem.Models;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication;
+using StoreSystem.Controllers;
 
 namespace StoreSystem
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		private IConfiguration Configuration { get; }
 
-        public IConfiguration Configuration { get; }
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<StoreContext>(opt =>
-                opt.UseInMemoryDatabase("StoreDB"));
-            services.AddControllers();
-        }
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllers().AddJsonOptions(options =>
+			{
+				options.JsonSerializerOptions.IgnoreNullValues = true;
+			});
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+			services.AddMvc(option => option.EnableEndpointRouting = false);
 
-            app.UseHttpsRedirection();
+			services.AddDbContext<StoreContext>(opt => opt.UseSqlServer(Configuration["Azure:ConnectionString"]));
+		}
 
-            app.UseRouting();
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			app.UseMvc();
 
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
-    }
+			app.UseAuthentication();
+			app.UseAuthorization();
+		}
+	}
 }
